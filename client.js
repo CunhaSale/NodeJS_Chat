b95108c881ea2f878aa7294f37c32020ee9a893f
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2017-10-02 13:06:41 -0300

    Core Modules - net

---
+++ client.js
@@ -0,0 +1,20 @@
+var net = require('net');
+
+var client = net.connect(3000);
+//Mandando mensagem para o servidor
+client.on('connect', function(){
+	client.write('Hello, I am the client!');	
+});
+//Recebendo mensagem do servidor
+client.on('data', function(message){
+	console.log(message.toString());
+});
+client.on('end', function(){
+	process.exit();
+});
+process.stdin.on('readable', function(){
+	var message = process.stdin.read();
+	if (!message) return;
+	message = message.toString().replace(/\n/, '');
+	client.write(message);
+});
